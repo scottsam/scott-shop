5460ed8248a9ae8b8b9dada38562a0e562d76809
scottsam <samuel_owulezi@yahoo.com>
2020-12-21 23:17:16 +0100

    first

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ const categoryRoute = require("./routes/category");
 const orderRoute = require("./routes/order");
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
@@ -43,6 +40,9 @@ app.use(cookieParser());
 //app.use(cors());
 app.use((req, res, next) => {
   res.set("Cache-Control", "no-store");
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
 
   next();
 });
