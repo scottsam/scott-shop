31032622cc7105aaa28fd273e4e5f1c0691fd514
scottsam <samuel_owulezi@yahoo.com>
2020-12-22 21:55:27 +0100

    m

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use("/", categoryRoute);
 app.use("/", orderRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use("/", express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build"));
   });
